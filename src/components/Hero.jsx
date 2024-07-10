0c203a715d0970b0e2b590bbe5dc1da21e703839
omrawat23 <omrawat23@gmail.com>
2024-07-10 23:06:31 +0530

    style: :art: hero section updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,9 +17,9 @@ const Hero = () => {
 
 
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      <div className='absolute lg:bottom-32 w-full flex justify-center items-center xxs:bottom-8 '>
         <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl mb-20 border-4 border-secondary flex justify-center items-start p-2'>
+          <div className='w-[35px] h-[64px] rounded-3xl mb-20 border-4 border-secondary flex justify-center items-start p-2 '>
             <motion.div
               animate={{
                 y: [0, 24, 0],
